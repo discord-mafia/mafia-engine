13c6f7c28808853286308cc5c66b54032ddfe7e9
Jack Nash <jacknash42@gmail.com>
2023-08-19 01:35:02 +1000

    split into monorepo

--- client/src/utils/trpc.ts
+++ client/src/utils/trpc.ts
@@ -1,3 +1,3 @@
 import { createTRPCReact } from '@trpc/react-query';
-import type { AppRouter } from '../../../src/router';
+import type { AppRouter } from '../../../server/src/router';
 export const trpc = createTRPCReact<AppRouter>();
