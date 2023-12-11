a3097956f22e46e9ca04db74a16a2f33204ed3d4
Jack Nash <jacknash42@gmail.com>
2023-12-12 00:06:41 +1000

    chore: delete unused commands utils

--- src/util/commands.ts
+++
@@ -1,5 +0,0 @@
-import { type SlashCommandBooleanOption } from 'discord.js';
-
-export function addHiddenOption(opt: SlashCommandBooleanOption) {
-	return opt.setName('hidden').setDescription('Whether to show hidden roles').setRequired(false);
-}
