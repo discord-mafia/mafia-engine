f4e8d030552d4ff3df1a4c350825989b5bc3d9b0
Jack Nash <jacknash42@gmail.com>
2023-12-13 04:27:25 +1000

    feat: add boilerplate REST API

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,14 @@
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 import config from './config';
+
+import { router as apiRouter } from './routers/apiRouter';
+
 export const app = express();
+app.use(express.json());
+app.use(cors());
+app.use('/api', apiRouter);
 
 const webAppPath = path.join(__dirname, '..', 'web', 'dist');
 app.use(express.static(webAppPath));
