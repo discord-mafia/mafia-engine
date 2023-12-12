f9185b35c8b5363e479f6d4fb66558ac1b8a47a8
Jack Nash <jacknash42@gmail.com>
2023-12-13 04:21:03 +1000

    revert: remove trpc

--- src/api.ts
+++ src/api.ts
@@ -1,19 +1,7 @@
 import express from 'express';
 import path from 'path';
 import config from './config';
-import * as trpcExpress from '@trpc/server/adapters/express';
-import { appRouter } from './routers';
-import { createContext } from '@structures/trpc';
-
 export const app = express();
-
-app.use(
-	'/trpc',
-	trpcExpress.createExpressMiddleware({
-		router: appRouter,
-		createContext,
-	})
-);
 
 const webAppPath = path.join(__dirname, '..', 'web', 'dist');
 app.use(express.static(webAppPath));
--- src/structures/trpc.ts
+++
@@ -1,9 +0,0 @@
-import { initTRPC } from '@trpc/server';
-import type * as trpcExpress from '@trpc/server/adapters/express';
-const t = initTRPC.create();
-
-export const router = t.router;
-export const publicProcedure = t.procedure;
-
-export const createContext = (_ctx: trpcExpress.CreateExpressContextOptions) => ({}); // no context
-export type Context = Awaited<ReturnType<typeof createContext>>;
