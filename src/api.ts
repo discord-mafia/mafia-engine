8f2be5cd4bd6898b7d7db2d4624e28873b061173
Jack Nash <jacknash42@gmail.com>
2023-12-13 03:36:36 +1000

    feat: add trpc to the server

--- src/api.ts
+++ src/api.ts
@@ -1,11 +1,22 @@
 import express from 'express';
 import path from 'path';
 import config from './config';
+import * as trpcExpress from '@trpc/server/adapters/express';
+import { appRouter } from './routers';
+import { createContext } from '@structures/trpc';
 
 export const app = express();
+
+app.use(
+	'/trpc',
+	trpcExpress.createExpressMiddleware({
+		router: appRouter,
+		createContext,
+	})
+);
+
 const webAppPath = path.join(__dirname, '..', 'web', 'dist');
 app.use(express.static(webAppPath));
-
 app.get('*', (req, res) => {
 	res.sendFile(path.join(webAppPath, 'index.html'));
 });
