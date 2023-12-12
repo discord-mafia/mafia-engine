1776d0d6d1d445086c04d25d112f60baa6bc702a
Jack Nash <jacknash42@gmail.com>
2023-12-13 03:22:48 +1000

    feat: add tailwind to the site

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './pages/main';
-import './styles/index.css';
+import './styles/reset.css';
+import './styles/tailwind.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
--- web/src/pages/main.tsx
+++ web/src/pages/main.tsx
@@ -1,10 +1,8 @@
-import '../styles/mainpage.css';
-
 function App() {
 	return (
 		<>
-			<h1>Mafia Engine</h1>
-			<p className="subtitle">This version of the site is currently in development.</p>
+			<h1 className="text-3xl font-bold underline text-center">Mafia Engine</h1>
+			<p className="text-center">This version of the site is currently in development.</p>
 		</>
 	);
 }
