dd39aaac802300847283639cfdae3a111c50317d
Jack Nash <jacknash42@gmail.com>
2023-12-15 18:23:17 +1000

    refactor: move citizenship cmds into its own group

--- src/events/commands/citizenship/register.ts
+++ src/events/commands/citizenship/register.ts
@@ -1,5 +1,5 @@
 import { SlashCommand } from '@structures/interactions/SlashCommand';
-import registerCitizenship from '../modals/registerCitizenship';
+import registerCitizenship from '../../modals/registerCitizenship';
 
 export default new SlashCommand('register').setDescription('Register for citizenship in Discord Mafia!').onExecute(async (i, ctx) => {
 	if (ctx.citizenship?.isRegistered) return i.reply({ content: 'You are already registered!', ephemeral: true });
