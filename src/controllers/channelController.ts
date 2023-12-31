a3eb28fde6e2023fcdd31c65d061b2a6861420d5
Jack Nash <jacknash42@gmail.com>
2024-01-01 09:41:14 +1000

    feat: add more category commands

--- src/controllers/channelController.ts
+++ src/controllers/channelController.ts
@@ -1,5 +1,29 @@
-import { type PermissionOverwriteOptions, type RoleResolvable, type TextChannel, type UserResolvable } from 'discord.js';
+import type { Guild, PermissionOverwriteOptions, RoleResolvable, TextChannel, UserResolvable } from 'discord.js';
+import { ChannelType } from 'discord.js';
 
 export async function editChannelPermission(channel: TextChannel, userOrRole: RoleResolvable | UserResolvable, payload: PermissionOverwriteOptions) {
-	await channel.permissionOverwrites.edit(userOrRole, payload);
+	return await channel.permissionOverwrites.edit(userOrRole, payload).catch(() => null);
+}
+
+export async function createCategory(guild: Guild, name: string) {
+	const category = await guild.channels
+		.create({
+			name,
+			type: ChannelType.GuildCategory,
+		})
+		.catch(() => null);
+
+	return category;
+}
+
+export async function createTextChannel(guild: Guild, name: string, parent?: string) {
+	const channel = await guild.channels
+		.create({
+			name,
+			type: ChannelType.GuildText,
+			parent,
+		})
+		.catch(() => null);
+
+	return channel;
 }
