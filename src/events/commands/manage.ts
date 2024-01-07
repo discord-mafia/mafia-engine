9fa2349d46fb48e1a57abf1a6208f5ead8079f57
Jack Nash <jacknash42@gmail.com>
2024-01-07 19:12:41 +1000

    feat: add inline permission checking in /manage

--- src/events/commands/manage.ts
+++ src/events/commands/manage.ts
@@ -2,15 +2,32 @@ import { getVoteCounter } from '@models/votecounter';
 import { genCreateVoteCountEmbed, genVoteCountEmbed } from '@views/votecounter';
 import { SlashCommand } from '@structures/interactions/SlashCommand';
 import { CustomError } from '@utils/errors';
+import { ChannelType, PermissionFlagsBits } from 'discord.js';
 
 export default new SlashCommand('manage')
 	.setDescription('Manage an part of the bot')
 	.set((cmd) => {
 		cmd.addSubcommand((sub) => sub.setName('votecount').setDescription('Manage the votecounter in this channel'));
+		cmd.addSubcommand((sub) => sub.setName('autolocker').setDescription('Manage the autolocker in this channel'));
+		cmd.setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels);
 	})
 	.onExecute(async (i) => {
 		if (!i.guild) return;
 		const subcommand = i.options.getSubcommand(true);
+
+		const channel = await i.guild.channels.fetch(i.channelId);
+		if (!channel || channel.type != ChannelType.GuildText)
+			return i.reply({
+				content: 'This channel is not a text channel OR this channel is not recognized by the bot',
+				ephemeral: true,
+			});
+
+		const canManageChannel = channel.permissionsFor(i.user.id)?.has('ManageChannels');
+		if (!canManageChannel)
+			return i.reply({
+				content: 'You do not have the required permissions in this channel to use this command',
+				ephemeral: true,
+			});
 
 		try {
 			if (subcommand === 'votecount') {
@@ -18,6 +35,11 @@ export default new SlashCommand('manage')
 				if (!vc) return i.reply({ ...genCreateVoteCountEmbed(), ephemeral: true });
 				const payload = genVoteCountEmbed(vc);
 				return i.reply({ ...payload, ephemeral: true });
+			} else if (subcommand === 'autolocker') {
+				return i.reply({
+					content: 'Autolocker',
+					ephemeral: true,
+				});
 			} else {
 				return i.reply({
 					content: 'Unknown subcommand',
