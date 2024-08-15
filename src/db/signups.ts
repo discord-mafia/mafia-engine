0a9c27b5980898df35dedcea1ad5aaf80dbbaf09
Jack Nash <jacknash42@gmail.com>
2024-08-15 13:43:45 +1000

    feat: add basic indexing to signup table

--- src/db/signups.ts
+++ src/db/signups.ts
@@ -1,5 +1,6 @@
 import {
 	boolean,
+	index,
 	integer,
 	pgTable,
 	serial,
@@ -8,38 +9,63 @@ import {
 } from 'drizzle-orm/pg-core';
 import { users } from './users';
 
-export const signups = pgTable('signups', {
-	id: serial('id').primaryKey(),
-	guild_id: varchar('guild_id', { length: 32 }).notNull(),
-	channel_id: varchar('channel_id', { length: 32 }).notNull(),
-	message_id: varchar('message_id', { length: 32 }).notNull(),
-	name: varchar('name', { length: 256 }).notNull(),
-	created_at: timestamp('created_at').notNull().defaultNow(),
-	updated_at: timestamp('updated_at').notNull().defaultNow(),
-});
+export const signups = pgTable(
+	'signups',
+	{
+		id: serial('id').primaryKey(),
+		guildId: varchar('guild_id', { length: 32 }).notNull(),
+		channelId: varchar('channel_id', { length: 32 }).notNull(),
+		messageId: varchar('message_id', { length: 32 }).notNull(),
+		name: varchar('name', { length: 256 }).notNull(),
+		createdAt: timestamp('created_at').notNull().defaultNow(),
+		updatedAt: timestamp('updated_at').notNull().defaultNow(),
+	},
+	(t) => {
+		return {
+			messageIdx: index('message_id_idx').on(t.messageId),
+		};
+	}
+);
 
-export const signup_cateogies = pgTable('signup_categories', {
-	id: serial('id').primaryKey(),
-	signup_id: integer('signup_id')
-		.notNull()
-		.references(() => signups.id),
-	name: varchar('name', { length: 256 }).notNull(),
-	button_name: varchar('button_name', { length: 256 }),
-	limit: integer('limit'),
-	is_focused: boolean('is_focused'),
-	is_hoisted: boolean('is_hoisted'),
-	is_locked: boolean('is_locked'),
-	created_at: timestamp('created_at').notNull().defaultNow(),
-	updated_at: timestamp('updated_at').notNull().defaultNow(),
-});
+export const signup_cateogies = pgTable(
+	'signup_categories',
+	{
+		id: serial('id').primaryKey(),
+		signupId: integer('signup_id')
+			.notNull()
+			.references(() => signups.id),
+		name: varchar('name', { length: 256 }).notNull(),
+		buttonName: varchar('button_name', { length: 256 }),
+		limit: integer('limit'),
+		isFocused: boolean('is_focused'),
+		isHoisted: boolean('is_hoisted'),
+		isLocked: boolean('is_locked'),
+		createdAt: timestamp('created_at').notNull().defaultNow(),
+		updatedAt: timestamp('updated_at').notNull().defaultNow(),
+	},
+	(t) => {
+		return {
+			signupIdx: index('signup_id_idx').on(t.signupId),
+		};
+	}
+);
 
-export const signup_user = pgTable('signup_user', {
-	id: serial('id').primaryKey(),
-	user_id: varchar('user_id', { length: 32 })
-		.notNull()
-		.references(() => users.id),
-	category_id: integer('category_id')
-		.notNull()
-		.references(() => signup_cateogies.id),
-	created_at: timestamp('created_at').notNull().defaultNow(),
-});
+export const signup_user = pgTable(
+	'signup_user',
+	{
+		id: serial('id').primaryKey(),
+		userId: varchar('user_id', { length: 32 })
+			.notNull()
+			.references(() => users.id),
+		categoryId: integer('category_id')
+			.notNull()
+			.references(() => signup_cateogies.id),
+		createdAt: timestamp('created_at').notNull().defaultNow(),
+	},
+	(t) => {
+		return {
+			userIdx: index('user_id_idx').on(t.userId),
+			categoryIdx: index('category_id_idx').on(t.categoryId),
+		};
+	}
+);
