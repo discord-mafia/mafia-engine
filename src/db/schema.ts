e98f854e614b2e6757dc4de992d3ba4ecbf06af7
Jack Nash <jacknash42@gmail.com>
2024-08-15 12:42:25 +1000

    feat: impl basic db query logic

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,7 @@
-import { pgTable, serial, varchar } from 'drizzle-orm/pg-core';
+import { pgTable, varchar } from 'drizzle-orm/pg-core';
 
 export const users = pgTable('users', {
-	id: serial('id').primaryKey(),
+	id: varchar('id', { length: 32 }).primaryKey(),
 	username: varchar('username', { length: 32 }),
 });
 
@@ -9,7 +9,7 @@ export type User = typeof users.$inferSelect;
 export type NewUser = typeof users.$inferInsert;
 
 export const guilds = pgTable('guilds', {
-	id: varchar('id', { length: 32 }),
+	id: varchar('id', { length: 32 }).primaryKey(),
 });
 
 export type Guild = typeof guilds.$inferSelect;
--- src/interactions/test.ts
+++ src/interactions/test.ts
@@ -1,11 +1,14 @@
 import { ButtonStyle } from 'discord.js';
 import { Button } from '../builders/button';
 import { SlashCommand } from '../builders/slashCommand';
+import { db } from '../controllers/database';
+import { users } from '../db/schema';
 
 export const test = new SlashCommand('test').onExecute(async (i) => {
+	const all_users = await db.select().from(users);
 	const btn = button.buildAsRow();
 	await i.reply({
-		content: 'Hello World!',
+		content: `There are ${all_users.length} users in the database.`,
 		components: [btn],
 		ephemeral: true,
 	});
@@ -13,6 +16,12 @@ export const test = new SlashCommand('test').onExecute(async (i) => {
 
 export const button = new Button('test')
 	.setStyle(ButtonStyle.Primary)
+	.setLabel('Create User')
 	.onExecute(async (i) => {
-		await i.reply({ content: 'Hello World!', ephemeral: true });
+		const inserted_user = await db.insert(users).values({
+			id: i.user.id,
+			username: i.user.username,
+		});
+		console.log(inserted_user);
+		await i.reply({ content: 'Created user', ephemeral: true });
 	});
