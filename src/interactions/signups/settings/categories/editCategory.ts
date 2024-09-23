956e724585ed4efeec519f8f432fb0638d6528f3
Jack Nash <jacknash42@gmail.com>
2024-09-23 11:01:59 +1000

    chore: add skeleton for edit category controls

--- src/interactions/signups/settings/categories/editCategory.ts
+++ src/interactions/signups/settings/categories/editCategory.ts
@@ -1,4 +1,10 @@
-import { ChannelType, EmbedBuilder } from 'discord.js';
+import {
+	ActionRowBuilder,
+	ButtonBuilder,
+	ButtonStyle,
+	ChannelType,
+	EmbedBuilder,
+} from 'discord.js';
 import { TextSelectMenu } from '../../../../builders/textSelectMenu';
 import { InteractionError } from '../../../../utils/errors';
 import {
@@ -6,6 +12,7 @@ import {
 	HydratedCategory,
 } from '../../../../db/signups';
 import { cleanUsername } from '../../../../utils/usernames';
+import { Button } from '../../../../builders/button';
 
 export const editCategoryMenu = new TextSelectMenu('edit-category')
 	.setMinValues(1)
@@ -34,8 +41,8 @@ export const editCategoryMenu = new TextSelectMenu('edit-category')
 		const category = signup.categories.find((c) => c.id == categoryId);
 		if (!category) throw new InteractionError('Invalid category');
 
-		const { embed } = genEditCategoryMenu(category);
-		await i.update({ embeds: [embed] });
+		const { embed, row } = genEditCategoryMenu(category);
+		await i.update({ embeds: [embed], components: [row] });
 	});
 
 export function genEditCategoryMenu(category: HydratedCategory) {
@@ -57,5 +64,65 @@ export function genEditCategoryMenu(category: HydratedCategory) {
 		value: users.length > 0 ? `${users.join('\n')}` : '> None',
 	});
 
-	return { embed };
+	const row = new ActionRowBuilder<ButtonBuilder>();
+
+	row.addComponents(
+		addUserButton.build(),
+		removeUserButton.build(),
+		cullUserButton.build(),
+		renameCategoryButton.build(),
+		limitChangeButton.build()
+	);
+
+	return { embed, row };
 }
+
+export const addUserButton = new Button('add-user-to-category')
+	.setLabel('Add Users')
+	.setStyle(ButtonStyle.Secondary)
+	.onExecute(async (i) => {
+		await i.reply({
+			content: 'This feature is not yet implemented',
+			ephemeral: true,
+		});
+	});
+
+export const removeUserButton = new Button('remove-user-from-category')
+	.setLabel('Remove User')
+	.setStyle(ButtonStyle.Secondary)
+	.onExecute(async (i) => {
+		await i.reply({
+			content: 'This feature is not yet implemented',
+			ephemeral: true,
+		});
+	});
+
+export const cullUserButton = new Button('cull-user-from-category')
+	.setLabel('Cull Users')
+	.setStyle(ButtonStyle.Secondary)
+	.onExecute(async (i) => {
+		await i.reply({
+			content: 'This feature is not yet implemented',
+			ephemeral: true,
+		});
+	});
+
+export const renameCategoryButton = new Button('rename-category')
+	.setLabel('Rename Category')
+	.setStyle(ButtonStyle.Secondary)
+	.onExecute(async (i) => {
+		await i.reply({
+			content: 'This feature is not yet implemented',
+			ephemeral: true,
+		});
+	});
+
+export const limitChangeButton = new Button('change-category-limit')
+	.setLabel('Change Category Limit')
+	.setStyle(ButtonStyle.Secondary)
+	.onExecute(async (i) => {
+		await i.reply({
+			content: 'This feature is not yet implemented',
+			ephemeral: true,
+		});
+	});
