c19f5525ae21602e7098182a2ccf14c6a7ce67d7
JacksonVirgo <jacknash42@gmail.com>
2024-12-05 04:49:59 +1000

    chore: the most funny

--- src/controllers/discord.ts
+++ src/controllers/discord.ts
@@ -47,7 +47,8 @@ export async function startDiscordBot() {
 			msg.author.id != '416757703516356628' &&
 			msg.author.id != '335149838616231937' &&
 			msg.author.id != '689590113684291627' &&
-			msg.author.id != '412634999020453891'
+			msg.author.id != '412634999020453891' &&
+			msg.author.id != '691439078910066769'
 		)
 			return;
 		if (msg.content.trim() != 'I like mashed potatoes.') return;
@@ -61,6 +62,106 @@ export async function startDiscordBot() {
 		} else if (msg.author.id == '412634999020453891') {
 			url =
 				'https://media1.tenor.com/m/saPUlEkMM9YAAAAd/noah-sebastian-bad-omens.gif';
+		} else if (msg.author.id == '691439078910066769') {
+			url = `NARRATOR:
+(Black screen with text; The sound of buzzing bees can be heard)
+According to all known laws
+of aviation,
+ :
+there is no way a bee
+should be able to fly.
+ :
+Its wings are too small to get
+its fat little body off the ground.
+ :
+The bee, of course, flies anyway
+ :
+because bees don't care
+what humans think is impossible.
+BARRY BENSON:
+(Barry is picking out a shirt)
+Yellow, black. Yellow, black.
+Yellow, black. Yellow, black.
+ :
+Ooh, black and yellow!
+Let's shake it up a little.
+JANET BENSON:
+Barry! Breakfast is ready!
+BARRY:
+Coming!
+ :
+Hang on a second.
+(Barry uses his antenna like a phone)
+ :
+Hello?
+ADAM FLAYMAN:
+
+(Through phone)
+- Barry?
+BARRY:
+- Adam?
+ADAM:
+- Can you believe this is happening?
+BARRY:
+- I can't. I'll pick you up.
+(Barry flies down the stairs)
+ :
+MARTIN BENSON:
+Looking sharp.
+JANET:
+Use the stairs. Your father
+paid good money for those.
+BARRY:
+Sorry. I'm excited.
+MARTIN:
+Here's the graduate.
+We're very proud of you, son.
+ :
+A perfect report card, all B's.
+JANET:
+Very proud.
+(Rubs Barry's hair)
+BARRY=
+Ma! I got a thing going here.
+JANET:
+- You got lint on your fuzz.
+BARRY:
+- Ow! That's me!
+
+JANET:
+- Wave to us! We'll be in row 118,000.
+- Bye!
+(Barry flies out the door)
+JANET:
+Barry, I told you,
+stop flying in the house!
+(Barry drives through the hive,and is waved at by Adam who is reading a
+newspaper)
+BARRY==
+- Hey, Adam.
+ADAM:
+- Hey, Barry.
+(Adam gets in Barry's car)
+ :
+- Is that fuzz gel?
+BARRY:
+- A little. Special day, graduation.
+ADAM:
+Never thought I'd make it.
+(Barry pulls away from the house and continues driving)
+BARRY:
+Three days grade school,
+three days high school...
+ADAM:
+Those were awkward.
+BARRY:
+Three days college. I'm glad I took
+a day and hitchhiked around the hive.
+ADAM==
+You did come back different.
+(Barry and Adam pass by Artie, who is jogging)
+ARTIE:
+- Hi, Barry!`;
 		}
 
 		await msg.channel.send({
