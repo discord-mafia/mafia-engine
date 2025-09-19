78132e5861a52038f3d235fcadfef5a4c525ae01
Jack Nash <jacknash42@gmail.com>
2025-09-19 11:31:52 +1000

    Update discord.ts

--- src/controllers/discord.ts
+++ src/controllers/discord.ts
@@ -58,110 +58,12 @@ export async function startDiscordBot() {
 			'https://images.unsplash.com/photo-1590152684852-ae3ccec52ac6?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
 		if (msg.author.id == '689590113684291627') {
 			url =
-				'https://media1.tenor.com/m/GJ2PsnsAmCMAAAAd/elmo-burning.gif';
+				'https://i.imgur.com/D1n73wX.gif';
 		} else if (msg.author.id == '412634999020453891') {
 			url =
 				'https://media1.tenor.com/m/saPUlEkMM9YAAAAd/noah-sebastian-bad-omens.gif';
 		} else if (msg.author.id == '691439078910066769') {
-			url = `NARRATOR:
-(Black screen with text; The sound of buzzing bees can be heard)
-According to all known laws
-of aviation,
- :
-there is no way a bee
-should be able to fly.
- :
-Its wings are too small to get
-its fat little body off the ground.
- :
-The bee, of course, flies anyway
- :
-because bees don't care
-what humans think is impossible.
-BARRY BENSON:
-(Barry is picking out a shirt)
-Yellow, black. Yellow, black.
-Yellow, black. Yellow, black.
- :
-Ooh, black and yellow!
-Let's shake it up a little.
-JANET BENSON:
-Barry! Breakfast is ready!
-BARRY:
-Coming!
- :
-Hang on a second.
-(Barry uses his antenna like a phone)
- :
-Hello?
-ADAM FLAYMAN:
-
-(Through phone)
-- Barry?
-BARRY:
-- Adam?
-ADAM:
-- Can you believe this is happening?
-BARRY:
-- I can't. I'll pick you up.
-(Barry flies down the stairs)
- :
-MARTIN BENSON:
-Looking sharp.
-JANET:
-Use the stairs. Your father
-paid good money for those.
-BARRY:
-Sorry. I'm excited.
-MARTIN:
-Here's the graduate.
-We're very proud of you, son.
- :
-A perfect report card, all B's.
-JANET:
-Very proud.
-(Rubs Barry's hair)
-BARRY=
-Ma! I got a thing going here.
-JANET:
-- You got lint on your fuzz.
-BARRY:
-- Ow! That's me!
-
-JANET:
-- Wave to us! We'll be in row 118,000.
-- Bye!
-(Barry flies out the door)
-JANET:
-Barry, I told you,
-stop flying in the house!
-(Barry drives through the hive,and is waved at by Adam who is reading a
-newspaper)
-BARRY==
-- Hey, Adam.
-ADAM:
-- Hey, Barry.
-(Adam gets in Barry's car)
- :
-- Is that fuzz gel?
-BARRY:
-- A little. Special day, graduation.
-ADAM:
-Never thought I'd make it.
-(Barry pulls away from the house and continues driving)
-BARRY:
-Three days grade school,
-three days high school...
-ADAM:
-Those were awkward.
-BARRY:
-Three days college. I'm glad I took
-a day and hitchhiked around the hive.
-ADAM==
-You did come back different.
-(Barry and Adam pass by Artie, who is jogging)
-ARTIE:
-- Hi, Barry!`;
+			url = `According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible. Yellow, black. Yellow, black. Yellow, black. Yellow, black. Ooh, black and yellow! Let's shake it up a little. Barry! Breakfast is ready! Coming! Hang on a second. Hello? Barry? Adam? Can you believe this is happening? I can't. I'll pick you up. Looking sharp. Use the stairs, Your father paid good money for those. Sorry. I'm excited. Here's the graduate. We're very proud of you, son. A perfect report card, all B's. Very proud. Ma! I got a thing going here. You got lint on your fuzz. Ow! That's me! Wave to us! We'll be in row 118,000. Bye! Barry, I told you, stop flying in the house! Hey, Adam. Hey, Barry. Is that fuzz gel? A little. Special day, graduation. Never thought I'd make it. Three days grade school, three days high school. Those were awkward. Three days college. I'm glad I took a day and hitchhiked around The Hive. You did come back different. Hi, Barry. Artie, growing a mustache? Looks good. Hear about Frankie? Yeah. You going to the funeral? No, I'm not going. Everybody knows, sting someone, you die. Don't waste it on a squirrel. Such a hothead. I guess he could have just gotten out of the way. I love this incorporating an amusement park into our day. That's why we don't need vacations. Boy, quite a bit of pomp under the circumstances. Well, Adam, today we are men. We are! Bee-men. Amen! Hallelujah! Students, faculty, distinguished bees, please welcome Dean Buzzwell. Welcome, New Hive City graduating class of 9:15. That concludes our ceremonies And begins your career at Honex Industries! Will we pick our job today? I heard it's just orientation. Heads up! Here we go. Keep your hands and antennas inside the tram at all times. Wonder what it'll be like? A little scary. Welcome to Honex, a division of Honesco and a part of the Hexagon Group. This is it! Wow. Wow. We know that you, as a bee, have worked your whole life to get to the point where you can work for your whole life. Honey begins when our valiant Pollen Jocks bring the nectar to The Hive. Our top-secret formula is automatically color-corrected, scent-adjusted and bubble-contoured into this soothing sweet syrup with its distinctive golden glow you know as... Honey! That girl was hot. She's my cousin! She is? Yes, we're all cousins. Right. You're right. At Honex, we constantly strive to improve every aspect of bee existence. These bees are stress-testing a new helmet technology. What do you think he makes? Not enough. Here we have our latest advancement, the Krelman. What does that do? Catches that little strand of honey that hangs after you pour it. Saves us millions. Can anyone work on the Krelman? Of course. Most bee jobs are small ones. But bees know that every small job, if it's done well, means a lot. But choose carefully because you'll stay in the job you pick for the rest of your life. The same job the rest of your life? I didn't know that. What's the difference? You'll be happy to know that bees, as a species, haven't had one day off in 27 million years. So you'll just work us to death? We'll sure try. Wow! That blew my mind! "What's the difference?" How can you say that? One job forever?`;
 		}
 
 		await msg.channel.send({
